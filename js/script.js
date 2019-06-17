2863a888609cdd7e22ffa962dc326fd93f1cba35
unknown <vargasmorenoalberto@gmail.com>
2019-06-17 10:18:12 -0500

    more code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 console.log("hi");
 const body = document.getElementsByName("body");
 const myName = document.getElementById("myName");
+
+function nameClicked(){
+    myName.classList.remove("animated", "flipInY");
+    myName.classList.add("animated", "flipInY");
+  }
+
+myName.addEventListener("click", nameClicked);
