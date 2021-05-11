3f1d5a79476355ae9936ebc474d306a0db3e7e0c
Tu Nombre <you@example.com>
2021-05-11 17:09:16 -0500

    :zap: improved projects section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log("hi");
+// Initialize Swiper 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        }
+    }
+})
